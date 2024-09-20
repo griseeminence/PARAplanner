8f79f8054dc914e902fabd06175280f3e04a315b
griseeminence <p.kolt080@gmail.com>
2024-09-20 19:19:21 +0200

    add project,resources forms,views,urls,html (DELETE DOES NOT WORK!)

--- to_do_list/para/urls.py
+++ to_do_list/para/urls.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
-from para.views import AreaCreateView, AreaDeleteView, AreaUpdateView, AreaListView, AreaDetailView
+from para.views import *
 
 app_name = 'para'
 
 urlpatterns = [
-    path('list/', AreaListView.as_view(), name='area_list'),
-    path('create/', AreaCreateView.as_view(), name='area_create'),
-    path('detail/<int:pk>/', AreaDetailView.as_view(), name='area_detail'),
-    path('update/<int:pk>/', AreaUpdateView.as_view(), name='area_update'),
-    path('delete/<int:pk>/', AreaDeleteView.as_view(), name='area_delete'),
+    path('area/list/', AreaListView.as_view(), name='area_list'),
+    path('area/create/', AreaCreateView.as_view(), name='area_create'),
+    path('area/detail/<int:pk>/', AreaDetailView.as_view(), name='area_detail'),
+    path('area/update/<int:pk>/', AreaUpdateView.as_view(), name='area_update'),
+    path('area/delete/<int:pk>/', AreaDeleteView.as_view(), name='area_delete'),
+    path('project/list/', ProjectListView.as_view(), name='project_list'),
+    path('project/create/', ProjectCreateView.as_view(), name='project_create'),
+    path('project/detail/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('project/update/<int:pk>/', ProjectUpdateView.as_view(), name='project_update'),
+    path('project/delete/<int:pk>/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('resource/list/', ResourceListView.as_view(), name='resource_list'),
+    path('resource/create/', ResourceCreateView.as_view(), name='resource_create'),
+    path('resource/detail/<int:pk>/', ResourceDetailView.as_view(), name='resource_detail'),
+    path('resource/update/<int:pk>/', ResourceUpdateView.as_view(), name='resource_update'),
+    path('resource/delete/<int:pk>/', ResourceDeleteView.as_view(), name='resource_delete'),
 ]
--- to_do_list/para/views.py
+++ to_do_list/para/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse
-from para.forms import AreaForm
+from para.forms import AreaForm, ProjectForm, ResourceForm
 from para.models import Area, Project, Resource, ResourceType
+
+# AREAS
+# AREAS
+# AREAS
+# AREAS
+# AREAS
+# AREAS
+# AREAS
+# AREAS
 
 
 class AreaListView(ListView):
@@ -41,6 +50,105 @@ class AreaDeleteView(DeleteView):
     template_name = 'para/area_confirm_delete.html'
     model = Area
     success_url = reverse_lazy('para:area_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Area, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+
+
+
+# PROJECTS
+# PROJECTS
+# PROJECTS
+# PROJECTS
+# PROJECTS
+# PROJECTS
+
+
+class ProjectListView(ListView):
+    template_name = 'para/project_list.html'
+    model = Project
+    ordering = '-id'
+    paginate_by = 4
+
+
+class ProjectDetailView(DetailView):
+    template_name = 'para/project_detail.html'
+    model = Project
+
+
+class ProjectCreateView(CreateView):
+    template_name = 'para/project_create.html'
+    model = Project
+    success_url = reverse_lazy('para:project_list')
+    form_class = ProjectForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ProjectUpdateView(UpdateView):
+    template_name = 'para/project_create.html'
+    model = Project
+    form_class = ProjectForm
+    success_url = reverse_lazy('para:project_list')
+
+
+class ProjectDeleteView(DeleteView):
+    template_name = 'para/project_confirm_delete.html'
+    model = Project
+    success_url = reverse_lazy('para:project_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        get_object_or_404(Area, pk=kwargs['pk'], author=request.user)
+        return super().dispatch(request, *args, **kwargs)
+
+# RESOURCES
+# RESOURCES
+# RESOURCES
+# RESOURCES
+# RESOURCES
+# RESOURCES
+# RESOURCES
+# RESOURCES
+
+
+class ResourceListView(ListView):
+    template_name = 'para/resource_list.html'
+    model = Resource
+    ordering = '-id'
+    paginate_by = 4
+
+
+class ResourceDetailView(DetailView):
+    template_name = 'para/resource_detail.html'
+    model = Resource
+
+
+class ResourceCreateView(CreateView):
+    template_name = 'para/resource_create.html'
+    model = Resource
+    success_url = reverse_lazy('para:resource_list')
+    form_class = ResourceForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ResourceUpdateView(UpdateView):
+    template_name = 'para/resource_create.html'
+    model = Resource
+    form_class = ResourceForm
+    success_url = reverse_lazy('para:resource_list')
+
+
+class ResourceDeleteView(DeleteView):
+    template_name = 'para/resource_confirm_delete.html'
+    model = Resource
+    success_url = reverse_lazy('para:resource_list')
 
     def dispatch(self, request, *args, **kwargs):
         get_object_or_404(Area, pk=kwargs['pk'], author=request.user)
